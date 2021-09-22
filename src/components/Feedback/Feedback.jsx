75f200f68f022f9005738c5853f339a95707cb6d
nickbron <77615583+nickbron@users.noreply.github.com>
2021-09-22 19:33:31 +0300

    create hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,60 +1,60 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Card } from './Feedback.styled';
 import Section from 'components/Sections/Sections';
 import Statistics from 'components/Statistics/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function Feedback(params) {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     const total = good + neutral + bad;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const percent = (good * 100) / this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const percent = (good * 100) / countTotalFeedback();
     return percent;
   };
 
-  incrementFeedback = feedback => {
-    this.setState(prevState => ({
-      [feedback]: prevState[feedback] + 1,
-    }));
+  const incrementFeedback = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const positiveFeedback = Math.round(this.countPositiveFeedbackPercentage());
-    const total = this.countTotalFeedback();
+  const positiveFeedback = Math.round(countPositiveFeedbackPercentage());
 
-    return (
-      <Card>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={{ good, neutral, bad }}
-            onLeaveFeedback={this.incrementFeedback}
-          />
-        </Section>
+  return (
+    <Card>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={{ good, neutral, bad }}
+          onLeaveFeedback={incrementFeedback}
+        />
+      </Section>
 
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positiveFeedback}
-          />
-        </Section>
-      </Card>
-    );
-  }
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={positiveFeedback}
+        />
+      </Section>
+    </Card>
+  );
 }
-
-export default Feedback;
